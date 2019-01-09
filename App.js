230ec42c156eaafba593c3a6d2e91f7ac353a76d
akhzar nazir <akhzarn@yahoo.com>
2019-01-09 07:56:09 +0500

    SectionList

--- App.js
+++ App.js
@@ -8,72 +8,50 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Alert,AsyncStorage, Button,
-        } from 'react-native';
+        SectionList,TouchableOpacity,Image} from 'react-native';
+
+var arrow_left=require('./arrow_left.png');
 
 type Props = {};
 export default class App extends Component<Props> {
 
-  constructor() {
-    super();
-
-    this.state={
-        // bookArray:[56,78,90,98,100],
-        // newVar:'0'
-      }
+  constructor(props) {
+    super(props);
   }
 
-  componentDidMount() {
-
-  AsyncStorage.getItem("login").then((value) => {
-  if (value == null) {
-    AsyncStorage.setItem('login', 'abc');
-  }else{
+  _sectionHeader = (info) => {
+    var txt = info.section.key;
+    return <Text
+      style={{ height: 80, textAlign: 'center', textAlignVertical: 'center', backgroundColor: 'green', color: 'white', fontSize: 30 }}>{txt}</Text>
   }
-}
-).done();
 
-AsyncStorage.getItem("password").then((value) => {
-if (value == null) {
-  AsyncStorage.setItem('password', '123');
-}else{
-}
-
-}
-).done();
-    // this.yasirFunction();
+  _renderItem = (info) => {
+    var txt = '  ' + info.item.title;
+    return <Text
+      style={{ height: 60, textAlignVertical: 'center', backgroundColor: "#ffffff", color: '#5C5C5C', fontSize: 15 }}>{txt}</Text>
   }
 
   render() {
+
+    var sections = [
+      { key: "ہمارے شہر", data: [{ title: "لاہور" }, { title: "کراچی" }, { title: "اسلام آباد" }] },
+      { key: "ہمارے کھیل", data: [{ title: "ہاکی" }, { title: "کرکٹ" }, { title: "کبڈی" }, { title: "گلی ڈنڈہ" }, { title: "کوئی اور" }] },
+      { key: "ہمارے تعلیمی ادارے", data: [{ title: "پنجاب یونیورسٹی" }, { title: "افرو ایشین" }] },
+      { key: "ہمارے مدرسے", data: [{ title: "جامعہ نعیمیہ" }, { title: "جامعہ اشرفیہ" }, { title: "جامعہ دارالعلوم" },{ title: "جامعہ بنوری" }, { title: "جامعہ ہندسیہ" }, { title: "جامعہ کراچی" }] },
+    ];
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}> Test AsyncStorage </Text>
+      <View style={{ flex: 1 }}>
+        <SectionList
+          sections={sections}
+          renderSectionHeader={this._sectionHeader}
+          renderItem={this._renderItem}
+
+          ItemSeparatorComponent={() => <View><Text>____________________</Text></View>}
+          ListHeaderComponent={() => <View style={{ backgroundColor: '#25B960', alignItems: 'center', height: 40 }}><Text style={{ fontSize: 18, color: '#ffffff' }}>سب سے اوپر</Text></View>}
+          ListFooterComponent={() => <View style={{ backgroundColor: '#25B960', alignItems: 'center', height: 40 }}><Text style={{ fontSize: 18, color: '#ffffff' }}>سب سے نیچے</Text></View>}
+        />
       </View>
     );
   }
-
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  textStyleNP:{
-  color:'white',
-  fontSize:25,
-  backgroundColor:'black',
-  // fontWeight:'bold',
-},
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
